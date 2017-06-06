5d3e501e8d1c1e33e62c0886613882a66863f962
Mojatto <daichiandroid1997@gmail.com>
2017-06-06 14:52:50 +0900

    initial commit

---
+++ ardrone_helpers/joy/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jupe/catkin_ws/src/ardrone_helpers/joy/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jupe/catkin_ws/src/ardrone_helpers/joy/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jupe/catkin_ws/src/ardrone_helpers/joy:$ROS_PACKAGE_PATH"
